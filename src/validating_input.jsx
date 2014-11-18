676d50c55e0c761831ac1dacf43ca728ebc357bc
Jan Muehlemann <jan.muehlemann@gmail.com>
2014-11-18 14:31:40 +0100

    optional feedback

--- src/validating_input.jsx
+++ src/validating_input.jsx
@@ -34,7 +34,7 @@ module.exports = React.createClass({
         onBlur={this.handleBlur}
         onChange={this.handleChange}
         ref="input"
-        hasFeedback
+        hasFeedback=this.props.hasFeedback ? true : false
         help={hasErrors ? _.first(this.props.errors) : null}
         bsStyle={hasErrors ? this.props.errorStyle : null} />
     );
